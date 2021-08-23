6a7883aaa4b13a991668d541ddd3770068745b9e
Josh <jecervantes15@gmail.com>
2021-08-22 23:08:06 -0700

    first commit

---
+++ Hello World/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    
+    public static void main(String[] args){
+        Message hello = new Message();
+        hello.sayHello();
+    }
+
+}
---
+++ Hello World/Message.java
@@ -0,0 +1,10 @@
+public class Message {
+    
+    public void sayHello(){
+        System.out.println("This is the message that should be outputted by the driver.");
+    }
+
+    public void sayGoodbye(){
+        System.out.println("Goodbye! It was nice seeing you!");
+    }
+}
